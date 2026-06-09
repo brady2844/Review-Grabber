ce9c799f99d41dbd635047ead7e8003e68e35a23
brady2844 <bradyscook@icloud.com>
2026-06-09 11:19:14 -0500

    Initial Commit

---
+++ HHG-Reviews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HHG-Reviews
+//
+//  Created by Brady Cook on 6/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HHG-Reviews/HHG_ReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HHG_ReviewsApp.swift
+//  HHG-Reviews
+//
+//  Created by Brady Cook on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct HHG_ReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
